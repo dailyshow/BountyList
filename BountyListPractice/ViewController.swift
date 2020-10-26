07cb2cdcaf6391da10539163e4ce7756b5a46e71
cis <chals001@naver.com>
2020-10-26 11:31:17 +0900

    새로운 연습 프로젝트 생성

---
+++ BountyListPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BountyListPractice
+//
+//  Created by isChoi on 2020/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
