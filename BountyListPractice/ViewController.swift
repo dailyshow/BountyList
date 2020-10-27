b7199befbafbe520e50fb67b7e923fcd5b1908af
cis <chals001@naver.com>
2020-10-27 11:18:57 +0900

    1. 오토레이아웃 적용 2. 이미지 추가 3. 새로운 view controller 추가 4. 세그웨이 연결(present modally)

--- BountyListPractice/ViewController.swift
+++ BountyListPractice/ViewController.swift
@@ -9,20 +9,53 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-
+    let nameList = ["brook","chopper","franky","luffy","nami","robin","sanji","zoro"]
+    let bountyPriceList = [33000000,50,44000000,300000000,16000000,80000000,77000000,120000000]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    // 연결한 세그웨이로 정보를 넘겨준다.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "showDetail" {
+            let detailVC = segue.destination as? DetailViewController
+            if let index = sender as? Int {
+                detailVC?.name = nameList[index]
+                detailVC?.bounty = bountyPriceList[index]
+            }
+        }
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return nameList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyPriceList[indexPath.row])"
         
         return cell
     }
+    
+    // cell 이 click 되었을 때 호출되는 메소드
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.row)")
+        
+        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+    }
 }
 
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+    
+}
