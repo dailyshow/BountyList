b2be57d3601dacad638fdbdc88917323ceaf2be3
dailyshow <chals001@naver.com>
2020-11-08 15:57:50 +0900

    1. UICollectionView 를 이용해서 한 줄에 여러개의 아이템을 배열시켰음 2. 아이템을 선택 했을 때 보이는 상세 화면에 애니메이션 처리하였음 (2가지 방법 연습. (1) constrain 를 이용한 방법. (2) view를 이용한 방법.)

--- BountyListPractice/DetailViewController.swift
+++ BountyListPractice/DetailViewController.swift
@@ -12,6 +12,8 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
     
     var viewModel = DetailViewModel()
     
@@ -19,6 +21,63 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
         updateUI()
+        prepareAnimation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        showAnimation()
+    }
+    
+    private func prepareAnimation() {
+        // constraint 를 이용한 방법.
+//        nameLabelCenterX.constant = view.bounds.width
+//        bountyLabelCenterX.constant = view.bounds.width
+        
+        // view 를 이용한 방법
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
+    }
+    
+    private func showAnimation() {
+        // view 이용한 animation 을 사용하기 위해 주석 처리
+//        nameLabelCenterX.constant = 0
+//        bountyLabelCenterX.constant = 0
+        
+        // 간단한 animation 적용
+//        UIView.animate(withDuration: 0.3) {
+//            self.view.layoutIfNeeded()
+//        }
+        
+        // 어느정도 상세한 animation을 적용할 수 있는 메소드
+//        UIView.animate(withDuration: 0.3, delay: 0.1, options: .curveEaseIn, animations: {self.view.layoutIfNeeded()}, completion: nil)
+
+        // view 이용한 animation 을 사용하기 위해 주석 처리
+        // 반동을 주는 animation
+//        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+//            self.view.layoutIfNeeded()
+//        }, completion: nil)
+        
+        // view 이용한 animation 을 사용하기 위해 주석 처리
+        // view 를 뒤집어주는 animation
+//        UIView.transition(with: imgView, duration: 0.2, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        
+        // view 를 이용한 animation 적용. nameLabel 과 bountyLabel 을 따로 움직이게 하기 위해 각각 적용.
+        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 1.0, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1
+        }, completion: nil)
+        
+        UIView.transition(with: imgView, duration: 1.0, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
     
     func updateUI() {
--- BountyListPractice/ViewController.swift
+++ BountyListPractice/ViewController.swift
@@ -7,7 +7,7 @@
 // MVVM 으로 분리 했음
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     let viewModel = BountyViewModel()
     
@@ -28,31 +28,51 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             }
         }
     }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    // UICollectionViewDataSource
+    // 몇개를 보여줄까요?
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numOfBountyInfoList
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as? ListCell else {
-            return UITableViewCell()
+    // 셀은 어떻게 표현할까요?
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else {
+            return UICollectionViewCell()
         }
-        
-        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.item)
+        // collectionView 에서는 item 순서를 인식해야 하기 때문에 indexPath.row가 아니라 indexPath.item 을 사용한다.
         cell.update(model: bountyInfo)
         
         return cell
     }
     
-    // cell 이 click 되었을 때 호출되는 메소드
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("\(indexPath.row)")
+    
+    // UICollectionViewDelegate
+    // 셀이 클릭되었을 때 어쩔까요?
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("\(indexPath.item)")
+        //
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
+    }
+    
+    // UICollectionViewDelegateFlowLayout
+    // 셀 사이즈를 계산할거다. collectionViewCell 한개의 크기가 단말기 한 줄에 2개 이상 보여주기 어렵다고 판단되면 하나만 보여주기도 하기 때문에 커스텀 크기 조절을 해주도록 한다.
+    // 다양한 크기의 디바이스에 대응하기 위해서는 크기 조절이 필수이다.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing: CGFloat = 10
+        let textAreaHeight: CGFloat = 65
         
-        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+        let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2
+        let height: CGFloat = width * 10/7 + textAreaHeight
+        
+        return CGSize(width: width, height: height)
     }
 }
 
-class ListCell: UITableViewCell {
+// collectionView 로 변경하고 나서 추가한 내용
+class GridCell: UICollectionViewCell {
+
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
